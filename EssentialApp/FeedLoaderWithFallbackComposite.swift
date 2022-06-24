2e4596990fa20e1e85ff532890b0675aa9a8f92b
Anil kumar <anilpal1392@gmail.com>
2022-06-24 07:52:54 +0530

    Move 'FeedLoaderWithFallbackComposite' to production

---
+++ EssentialApp/FeedLoaderWithFallbackComposite.swift
@@ -0,0 +1,33 @@
+//
+//  FeedLoaderWithFallbackComposite.swift
+//  EssentialApp
+//
+//  Created by 13401027 on 24/06/22.
+//
+
+import Foundation
+import EssentialFeed
+
+// RemoteWithLocalFallbackFeedLoader
+
+public class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
+    
+    public init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+                
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
+    }
+}
--- EssentialAppTests/RemoteWithLocalFallbackFeedloaderTests.swift
+++ EssentialAppTests/RemoteWithLocalFallbackFeedloaderTests.swift
@@ -5,34 +5,9 @@
 //  Created by 13401027 on 23/06/22.
 //
 
-import Foundation
 import XCTest
 import EssentialFeed
-
-class FeedLoaderWithFallbackComposite: FeedLoader {
-    // RemoteWithLocalFallbackFeedLoader
-    private let primary: FeedLoader
-    private let fallback: FeedLoader
-    
-    init(primary: FeedLoader, fallback: FeedLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-                
-            case .failure:
-                self?.fallback.load(completion: completion)
-            }
-        }
-    }
-    
-}
-
+import EssentialApp
 
 // RemoteWithLocalFallbackFeedLoaderTests
 
