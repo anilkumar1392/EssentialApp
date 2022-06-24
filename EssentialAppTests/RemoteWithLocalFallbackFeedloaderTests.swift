df4f1aad4f420a719d3a82092451aa5895ea4501
Anil kumar <anilpal1392@gmail.com>
2022-06-24 07:49:00 +0530

    'FeedLoaderWithFallbackComposite.load' delivers error on both primary and fallback failure

--- EssentialAppTests/RemoteWithLocalFallbackFeedloaderTests.swift
+++ EssentialAppTests/RemoteWithLocalFallbackFeedloaderTests.swift
@@ -56,7 +56,12 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
         
         expect(sut, toCompleteWith: .success(fallbackFeed))
+    }
+    
+    func test_load_deliverErrorOnBothPrimaryAndFallbackLoaderFailure() {
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .failure(anyNSError()))
 
+        expect(sut, toCompleteWith: .failure(anyNSError()))
     }
 }
 
