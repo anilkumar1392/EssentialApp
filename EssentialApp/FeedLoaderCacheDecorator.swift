6e47bf3dc4eeb7d8da9291e07cde879397690a6d
Anil kumar <anilpal1392@gmail.com>
2022-06-24 16:32:09 +0530

    Move 'RemoteFeedLoaderDecorator' to production

---
+++ EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,32 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by 13401027 on 24/06/22.
+//
+
+import Foundation
+import EssentialFeed
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+//            if let feed = try? result.get() {
+//                self?.cache.save(feed) { _ in }
+//            }
+
+            completion(result.map { feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
