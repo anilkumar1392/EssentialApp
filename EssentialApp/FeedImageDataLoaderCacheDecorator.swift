616608fd3901ba721753e989c5af02d718430832
Anil kumar <anilpal1392@gmail.com>
2022-06-25 14:42:57 +0530

    Move FeedImageDataLoaderCacheDecorator to production

---
+++ EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -0,0 +1,29 @@
+//
+//  FeedImageDataLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by 13401027 on 25/06/22.
+//
+
+import Foundation
+import EssentialFeed
+
+public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+    
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataTaskLoader {
+        return decoratee.loadImageData(from: url, completion: { [weak self] result in
+            completion(result
+                .map { data in
+                    self?.cache.save(data, for: url) { _ in }
+                    return data
+                })
+        })
+    }
+}
--- EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -9,26 +9,6 @@ import Foundation
 import XCTest
 import EssentialFeed
 
-final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
-    private let decoratee: FeedImageDataLoader
-    private let cache: FeedImageDataCache
-    
-    init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataTaskLoader {
-        return decoratee.loadImageData(from: url, completion: { [weak self] result in
-            completion(result
-                .map { data in
-                    self?.cache.save(data, for: url) { _ in }
-                    return data
-                })
-        })
-    }
-}
-
 class FeedImageDataLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTestCase {
     
     func test_init_doesNotLoadImageData() {
