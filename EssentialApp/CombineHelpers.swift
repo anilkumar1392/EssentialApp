b32ab3585d6d632955ed45e53afd1bd2f2ac052d
Anil kumar <anilpal1392@gmail.com>
2022-06-30 09:12:29 +0530

    Replace FeedLoader compositions with combine

---
+++ EssentialApp/CombineHelpers.swift
@@ -0,0 +1,98 @@
+//
+//  CombineHelpers.swift
+//  EssentialApp
+//
+//  Created by 13401027 on 30/06/22.
+//
+
+import Foundation
+import Combine
+import EssentialFeed
+
+extension FeedLoader {
+    public typealias Publisher = AnyPublisher<[FeedImage], Error>
+    
+    public func loadPublisher() -> Publisher {
+        Deferred {
+            Future { completion in
+                self .load(completion: completion)
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+
+// Configure with decorator
+extension Publisher where Output == [FeedImage] {
+    func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
+//        map { feed in
+//            cache.saveIgnoringResult(feed)
+//            return feed
+//        }
+//        .eraseToAnyPublisher()
+        
+//        handleEvents(receiveOutput: { feed in
+//            cache.saveIgnoringResult(feed)
+//        })
+//        .eraseToAnyPublisher()
+
+        handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
+    }
+}
+
+// Configure for fallback
+extension Publisher {
+    func failure(to fallbackPublisher: @escaping () -> AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
+        self.catch { _ in fallbackPublisher() }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher {
+    func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
+        receive(on: DispatchQueue.immediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
+    }
+}
+
+extension DispatchQueue {
+    static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueSchedular {
+        ImmediateWhenOnMainQueueSchedular()
+    }
+    
+    struct ImmediateWhenOnMainQueueSchedular: Scheduler {
+
+        typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
+        
+        typealias SchedulerOptions = DispatchQueue.SchedulerOptions
+        
+        /// This scheduler’s definition of the current moment in time.
+        var now: Self.SchedulerTimeType {
+            DispatchQueue.main.now
+        }
+
+        /// The minimum tolerance allowed by the scheduler.
+        var minimumTolerance: Self.SchedulerTimeType.Stride {
+            DispatchQueue.main.minimumTolerance
+        }
+
+        /// Performs the action at the next possible opportunity.
+        func schedule(options: Self.SchedulerOptions?, _ action: @escaping () -> Void) {
+            guard Thread.isMainThread else {
+                return DispatchQueue.main.schedule(options: options, action)
+            }
+            
+            action()
+        }
+
+        /// Performs the action at some time after the specified date.
+        func schedule(after date: Self.SchedulerTimeType, tolerance: Self.SchedulerTimeType.Stride, options: Self.SchedulerOptions?, _ action: @escaping () -> Void) {
+            DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
+        }
+
+        /// Performs the action at some time after the specified date, at the specified frequency, optionally taking into account tolerance if possible.
+        func schedule(after date: Self.SchedulerTimeType, interval: Self.SchedulerTimeType.Stride, tolerance: Self.SchedulerTimeType.Stride, options: Self.SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable {
+            DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, options: options, action)
+        }
+    }
+}
